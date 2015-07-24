bf42b5478935eecbcaf318378815839ba629e2f5
Siddartha Naidu <sid>
2015-07-23 23:45:26 -0500

    Create portfolio page.

---
+++ portfolio/init.js
@@ -0,0 +1,30 @@
+$(function () {
+var menuItems = $("nav.ui.menu .item");
+menuItems
+.click(function (i) {
+    var section = this.getAttribute("data-section");
+    console.log("Data section = " + section);
+    var offset = $("#" + section).offset();
+    $("html, body").animate({
+      scrollTop: offset.top,
+    });
+    $(this)
+    .addClass("active")
+    .closest(".ui.menu")
+    .find(".item")
+    .not($(this))
+    .removeClass("active");
+  });
+var activate = function() {
+  menuItems.filter(".active").removeClass("active");
+  $('nav [data-section="' + this.id + '"]').addClass("active");
+};
+$("main section")
+.visibility({
+    observeChanges: false,
+    once: false,
+    offset: $(window).height() / 2,
+    onTopPassed: activate,
+    onBottomPassedReverse: activate
+  });
+});
